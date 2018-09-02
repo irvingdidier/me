388c3b76862c4360691e948bb7ab9202fc0ea406
Irving Didier <bider88@gmail.com>
2018-09-02 11:34:46 -0500

    Agregando el menu en móvil

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+var bntMenu = document.getElementById('btn-menu');
+var menu = document.getElementById('menu');
+
+function toggleMenu() {
+    bntMenu.classList.toggle('change');
+    menu.classList.toggle('active');
+}
+
+function hideMenu() {
+    bntMenu.classList.remove('change');
+    menu.classList.remove('active');
 }
